8520536d92b880b8d9b0371937b28df31ca04645
Martin Capodici <martin.capodici@gmail.com>
2016-03-11 21:59:34 +1100

    Make content script load later to capture changes. This may make it work better with Jira for example. Also check it with a linter.

--- src/content.js
+++ src/content.js
@@ -1,35 +1,40 @@
-chrome.storage.sync.get(null, function(dict){
-  var links = document.links;
-  for(var i = 0; i < links.length; i++) {
-    var link = links[i];
-    if (dict[link])
-    {
-      var data = JSON.parse(dict[link]);
+var tid = setInterval( function () {
+  if ( document.readyState !== 'complete' ) return;
+  clearInterval( tid );
 
-      if (data)
+  chrome.storage.sync.get(null, function(dict){
+    var links = document.links;
+    for(var i = 0; i < links.length; i++) {
+      var link = links[i].href;
+      if (dict[link])
       {
-        if (data["d"])
+        var data = JSON.parse(dict[link]);
+
+        if (data && data.n)
         {
-          var tickImage = document.createElement("img");
-          tickImage.src = chrome.extension.getURL('images/tick16.png');
-          tickImage.alt = 'Done';
-          tickImage.title = 'You have completed this note';
-          tickImage.style.padding = '2px';
-          insertAfter(links[i], tickImage);
+          if (data.d)
+          {
+            var tickImage = document.createElement("img");
+            tickImage.src = chrome.extension.getURL('images/tick16.png');
+            tickImage.alt = 'Done';
+            tickImage.title = 'You have completed this note';
+            tickImage.style.padding = '2px';
+            insertAfter(links[i], tickImage);
 
-          links[i].style["text-decoration"] = 'line-through';
+            links[i].style["text-decoration"] = 'line-through';
+          }
+
+          var noteImage = document.createElement("img");
+          noteImage.src = chrome.extension.getURL('images/icon19.png');
+          noteImage.alt = 'Note';
+          noteImage.title = 'You have made a note on this page: ' + data.n;
+          noteImage.style.padding = '2px 2px 2px 8px';
+          insertAfter(links[i], noteImage);
         }
-
-        var noteImage = document.createElement("img");
-        noteImage.src = chrome.extension.getURL('images/icon19.png');
-        noteImage.alt = 'Note';
-        noteImage.title = 'You have made a note on this page: ' + data["n"];
-        noteImage.style['padding'] = '2px 2px 2px 8px';
-        insertAfter(links[i], noteImage);
       }
     }
-  }
-});
+  });
+}, 100 );
 
 function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
